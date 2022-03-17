fa8bc0b0651a7d3d64e83d075c73d197cec7dfa4
bdebicki <bartosz.debicki@zoin.pl>
2022-03-17 18:52:45 +0100

    add mocked data + get async data + restructurize folders

--- src/js/views/App.jsx
+++ src/js/views/App.jsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 
 // import '@/styles/index.scss';
 
 const App = () => {
+  useEffect(() => {
+    const getFestivalData = async () => {
+      const { data } = await axios.get('./assets/festivalEditions.json');
+
+      console.log(data);
+    }
+
+    getFestivalData();
+  });
+
+
   return (
     <div>
       app
